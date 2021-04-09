9e7d3c2656eb321467807ae357c6b1fa1ef309d5
David Bestue <davidsanchezbestue@hotmail.com>
2021-04-09 02:07:39 +0200

    update

--- task/pavlovia_/task_lastrun.py
+++ task/pavlovia_/task_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.3),
-    on abril 09, 2021, at 00:33
+    on abril 09, 2021, at 02:06
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -78,13 +78,13 @@ else:
 # create a default keyboard (e.g. to check for escape)
 defaultKeyboard = keyboard.Keyboard()
 
-# Initialize components for Routine "Instructions_2"
-Instructions_2Clock = core.Clock()
+# Initialize components for Routine "Instructions"
+InstructionsClock = core.Clock()
 text_3 = visual.TextStim(win=win, name='text_3',
-    text='¡Bienvenido!\n\nEn este test de working memory tendrás que \nmemorizar la posición de los diferentes colores.\n\n\nEl número de itema a recordar irá variando!\n\n\nPulsa "space"para continuar',
+    text='¡Bienvenido!\n\nEn este test de working memory tendrás que memorizar la posición de los diferentes colores.\n\nDespués de memorizarlos deberás hacer click en la posición del color que te indiquemos (el color aparecerá encima de la curz central acabado el tiempo de memorización).\n\nAunque no estés seguro, arriésgate!\n\nEl número de items a recordar así como el tiempo de memorización irán variando para hacerlo más complicado, ¡no te asustes!\n\nLa durada total es de entre 10-15min\n\nCuando lo tengas claro, pulsa  "space"para empezar.',
     font='Calibri',
-    pos=(-0.25, 0), height=0.025, wrapWidth=None, ori=0, 
-    color='white', colorSpace='rgb', opacity=1, 
+    pos=(-0.1, 0.025), height=0.04, wrapWidth=None, ori=0, 
+    color='black', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=0.0);
 key_resp_3 = keyboard.Keyboard()
@@ -247,7 +247,7 @@ text_2 = visual.TextStim(win=win, name='text_2',
     text='¡Muchas gracias por participar!\n\n\nPara salir, pulsa "space"\n',
     font='Calibri',
     pos=(0, 0), height=0.025, wrapWidth=None, ori=0, 
-    color='white', colorSpace='rgb', opacity=1, 
+    color='black', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=0.0);
 key_resp_2 = keyboard.Keyboard()
@@ -256,13 +256,13 @@ key_resp_2 = keyboard.Keyboard()
 globalClock = core.Clock()  # to track the time since experiment started
 routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
-# ------Prepare to start Routine "Instructions_2"-------
+# ------Prepare to start Routine "Instructions"-------
 # update component parameters for each repeat
 key_resp_3.keys = []
 key_resp_3.rt = []
 # keep track of which components have finished
-Instructions_2Components = [text_3, key_resp_3]
-for thisComponent in Instructions_2Components:
+InstructionsComponents = [text_3, key_resp_3]
+for thisComponent in InstructionsComponents:
     thisComponent.tStart = None
     thisComponent.tStop = None
     thisComponent.tStartRefresh = None
@@ -272,15 +272,15 @@ for thisComponent in Instructions_2Components:
 # reset timers
 t = 0
 _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-Instructions_2Clock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+InstructionsClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
 frameN = -1
 continueRoutine = True
 
-# -------Run Routine "Instructions_2"-------
+# -------Run Routine "Instructions"-------
 while continueRoutine:
     # get current time
-    t = Instructions_2Clock.getTime()
-    tThisFlip = win.getFutureFlipTime(clock=Instructions_2Clock)
+    t = InstructionsClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=InstructionsClock)
     tThisFlipGlobal = win.getFutureFlipTime(clock=None)
     frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
     # update/draw components on each frame
@@ -328,7 +328,7 @@ while continueRoutine:
     if not continueRoutine:  # a component has requested a forced-end of Routine
         break
     continueRoutine = False  # will revert to True if at least one component still running
-    for thisComponent in Instructions_2Components:
+    for thisComponent in InstructionsComponents:
         if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
             continueRoutine = True
             break  # at least one component has not yet finished
@@ -337,8 +337,8 @@ while continueRoutine:
     if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
         win.flip()
 
-# -------Ending Routine "Instructions_2"-------
-for thisComponent in Instructions_2Components:
+# -------Ending Routine "Instructions"-------
+for thisComponent in InstructionsComponents:
     if hasattr(thisComponent, "setAutoDraw"):
         thisComponent.setAutoDraw(False)
 thisExp.addData('text_3.started', text_3.tStartRefresh)
@@ -352,7 +352,7 @@ if key_resp_3.keys != None:  # we had a response
 thisExp.addData('key_resp_3.started', key_resp_3.tStartRefresh)
 thisExp.addData('key_resp_3.stopped', key_resp_3.tStopRefresh)
 thisExp.nextEntry()
-# the Routine "Instructions_2" was not non-slip safe, so reset the non-slip timer
+# the Routine "Instructions" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
 # set up handler to look after randomisation of conditions etc
@@ -580,14 +580,6 @@ for thisTrial in trials:
             win.timeOnFlip(mouse, 'tStartRefresh')  # time at next scr refresh
             mouse.status = STARTED
             prevButtonState = mouse.getPressed()  # if button is down already this ISN'T a new click
-        if mouse.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > mouse.tStartRefresh + 10-frameTolerance:
-                # keep track of stop time/frame for later
-                mouse.tStop = t  # not accounting for scr refresh
-                mouse.frameNStop = frameN  # exact frame index
-                win.timeOnFlip(mouse, 'tStopRefresh')  # time at next scr refresh
-                mouse.status = FINISHED
         if mouse.status == STARTED:  # only update if started and not finished!
             buttons = mouse.getPressed()
             if buttons != prevButtonState:  # button state changed?
@@ -1005,7 +997,7 @@ while continueRoutine:
     
     # *key_resp_2* updates
     waitOnFlip = False
-    if key_resp_2.status == NOT_STARTED and tThisFlip >= 15-frameTolerance:
+    if key_resp_2.status == NOT_STARTED and tThisFlip >= 2-frameTolerance:
         # keep track of start time/frame for later
         key_resp_2.frameNStart = frameN  # exact frame index
         key_resp_2.tStart = t  # local t and not account for scr refresh
